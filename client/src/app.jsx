7f905c28ae82a38bbc8f191c3db53bab76128209
Zach Timmerman <zachary.timmerman@gmail.com>
2023-03-15 22:05:08 -0400

    Integrate DaisyUI library

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -10,7 +10,7 @@ const App = () => {
 
   return (
     <div>
-      <h1 className="text-3xl font-bold underline"> Hello, World!</h1>
+      <h1 className="text-3xl font-bold"> Hello, World!</h1>
 
       {/* < Overview /> */}
       {/* < Related /> */}

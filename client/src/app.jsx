5d7047ce230890d8ce7ebb73b93d530e0efc23f4
Stephen Perkins <stperk21@gmail.com>
2023-03-17 12:09:18 -0400

    App.jsx minor updates

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 import { useState } from "react";
 import ReactDOM from "react-dom";
+//import Overview from "./overview/...."
 import Related from "./related/components/related_widget.jsx"
 // import Overview from "./overview/index.jsx"
-import Ratings from "./ratings/Ratings.jsx";
+//import Related from "./related/...."
+//import Ratings from "./ratings/...."
 //import Questions&Answers from "./q&a/...."
 
-
-
-const App = ({initialProduct}) => {
+const App = () => {
 // test
 
   const [cart, setCart] = useState([])
-  const [product, setProduct] = useState(initialProduct)
-
-  const updateProduct = () => {
-    // handles clicks on new products
-  }
 
   return (
     <div>
       <h1 className="text-3xl font-bold"> Hello, World!</h1>
 
-      {/* <Overview cart={cart}/> */}
+      {/* < Overview /> */}
       < Related />
-      <Ratings product={product}>Ratings</Ratings>
+      {/* <Overview cart={cart}/>*/ }
+      {/* < Related /> */}
+      {/* < Ratings /> */}
       {/* < Questions&Answers /> */}
     </div>
   )

e21a003c8feb16922d55b8285613e9da1f080042
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-03-17 11:19:04 -0400

    Resolved merge conflcits with app.jsx

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import { useState } from "react";
 import ReactDOM from "react-dom";
@@ -6,28 +5,20 @@ import ReactDOM from "react-dom";
 import Related from "./related/components/related_widget.jsx"
 // import Overview from "./overview/index.jsx"
 //import Related from "./related/...."
-//import Ratings from "./ratings/...."
-//import Questions&Answers from "./q&a/...."
-=======
-import React, {useState, useEffect} from "react";
-import ReactDOM from "react-dom";
 import Ratings from "./ratings/Ratings.jsx";
-import axios from 'axios';
->>>>>>> b5d9f97 (Set up ratings architecture, edits to app.jsx and index.jsx)
+//import Questions&Answers from "./q&a/...."
+
 
 
 const App = ({product}) => {
 // test
 
-<<<<<<< HEAD
-  const [cart, setCart] = useState([])
-=======
+const [cart, setCart] = useState([])
 const [product, setProduct] = useState(product)
 
 const updateProduct = () => {
   // handles clicks on new products
 }
->>>>>>> b5d9f97 (Set up ratings architecture, edits to app.jsx and index.jsx)
 
   return (
     <div>

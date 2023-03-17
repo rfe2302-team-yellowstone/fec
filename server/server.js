715fe1db091dde7def6ce46ced2a839eab450bb5
Stephen Perkins <stperk21@gmail.com>
2023-03-16 21:27:00 -0400

    Add API GET routes

--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const router = require('./routes.js').router
 
 const app = express();
 let port = process.env.PORT || 3000;
 
 const morgan = require('morgan')
 
-// Serves up all static and generated assets in ../client/dist.
+// Serves up all static and generated assets in ../client/dist
 app.use(express.static(path.join(__dirname, "/../client/dist")));
 app.use(express.json())
 app.use(morgan('tiny'))
+
+app.use('/', router)
 
 app.listen(port, (err) => {
   if (err) {

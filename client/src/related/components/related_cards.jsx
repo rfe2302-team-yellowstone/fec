c45dc476388a50a51fccbe55be4e6d37c244cf01
Jose <jose.umana@icloud.com>
2023-03-21 11:45:39 -0400

    Modified Component naming conventions and file location. Added modal to related card and small changes to card size and button color

--- client/src/related/components/related_cards.jsx
+++ client/src/related/components/related_cards.jsx
@@ -8,14 +8,14 @@ import axios from "axios"
 const RelatedCard = ({product}) => {
     //Hooks
     const [relatedItems, setRelatedItems] = useState([]);
-
+    const [openModal, setOpenModal] = useState(false);
+    const [comparedProduct, setComparedProduct] = useState('');
 
 
     //General product information
     let fetchingRelatedProducts = () => axios.get(`http://localhost:3000/products/${product.id}/related`)
     .then(response => {
       let relatedIds = response.data;
-      let relatedProducts = [];
       let getProductInfo = Promise.all(relatedIds.map(id =>
         axios.get(`http://localhost:3000/products/${id}`)
         .then(res => res.data)
@@ -35,7 +35,6 @@ const RelatedCard = ({product}) => {
       ).then((res) => {
         let productInfo = res[0];
         let productStyle = res[1];
-        console.log(productStyle)
         let productPhoto = [];
 
         for(let i = 0; i < productStyle.length; i++) {
@@ -50,15 +49,15 @@ const RelatedCard = ({product}) => {
               photoObj['thumbnail_url'] = resultObj[0].photos[0].thumbnail_url;
               photoObj['url'] = resultObj[0].photos[0].url;
             }
-          };
+          }
           productPhoto.push(photoObj);
-        };
+        }
 
-        productInfo.forEach(product =>{
+        productInfo.forEach(producto =>{
           for(let i = 0; i < productPhoto.length; i++) {
-            if(product.id == productPhoto[i].id) {
-              product['thumbnail_url'] = productPhoto[i].thumbnail_url;
-              product['url'] = productPhoto[i].url;
+            if(producto.id == productPhoto[i].id) {
+              producto['thumbnail_url'] = productPhoto[i].thumbnail_url;
+              producto['url'] = productPhoto[i].url;
             }
           }
 
@@ -76,15 +75,29 @@ const RelatedCard = ({product}) => {
       fetchingRelatedProducts();
     }, [product])
 
-    console.log(relatedItems)
+
+
+    const modalHandler = (event) =>{
+
+      relatedItems.forEach(item => {
+        if (item.id == event.target.id) {
+          setComparedProduct(item);
+        }
+      })
+
+      setOpenModal(!openModal)
+    }
+
     const cards = relatedItems.map((card) =>
 
       <div key={card.id} className="carousel-item rounded-box  w-1/2">
+
           <div className="card w-96 bg-base-100 shadow-xl">
 
           <figure className="relative w-full h-3/4">
-            <img className="w-full h-3/4" src={card.url}/>
-            <label htmlFor="my-modal-4" className = 'absolute top-2 right-0 btn-circle '><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg></label>  </figure>
+
+            <img className="w-full h-full" src={card.url}/>
+            <label onClick={modalHandler} id = {card.id} className = 'btn btn-ghost absolute top-2 right-0  '><svg id ={card.id}xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg></label>  </figure>
             <div className="card-body">
               <h2 className="card-title">{card.name}</h2>
               <p>{card.description}</p>
@@ -95,7 +108,7 @@ const RelatedCard = ({product}) => {
     );
 
     return (
-      <>{cards}</>
+      <>{cards} <Comparison openModal={openModal} setOpenModal={setOpenModal} product={product} relatedItems = {relatedItems} comparedProduct={comparedProduct}/> </>
     )
 }
 

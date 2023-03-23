d5a54b632dcd87756a43c3f7343a162b7e7e44e4
Zach Timmerman <zachary.timmerman@gmail.com>
2023-03-23 12:05:17 -0400

    Uncomment module components

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -50,12 +50,12 @@ const App = ({initialProduct}) => {
 
   return (
     <div>
-      {/* <h1 className="text-3xl font-bold mb-40"> Hello, World!</h1> */}
+      <h1 className="text-3xl font-bold mb-40"> Hello, World!</h1>
 
-      {/* <Overview cart={cart} product={product} handleSearch={handleSearch}/> */}
-      {/* <Related product={product}/> */}
+      <Overview cart={cart} product={product} handleSearch={handleSearch}/>
+      <Related product={product}/>
       <Ratings product={product}>Ratings</Ratings>
-      {/* <QAndA product={product}/> */}
+      <QAndA product={product}/>
     </div>
   )
 

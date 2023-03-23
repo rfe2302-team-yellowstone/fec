b2fc87ab30dd7144bdbfcc199448b11ba4897d84
Zack Freeman <73715945+zfreeman341@users.noreply.github.com>
2023-03-23 15:51:57 -0400

    Finished characteristics component, added infinite scroll for reviews

--- client/src/ratings/components/Reviews.jsx
+++ client/src/ratings/components/Reviews.jsx
@@ -17,22 +17,26 @@ const Reviews = ({product, reviews, setReviews}) => {
     setOrder(reviews)
   }, [reviews])
 
-  useEffect(() => {
-    console.log(order, '-----ORDER------')
-  }, [order])
+  // useEffect(() => {
+  //   console.log(order, '-----ORDER------')
+  // }, [order])
 
   return (
-    <div className="mb-4">
-    <Sort reviews={reviews} setReviews={setReviews} order={order} setOrder={setOrder}></Sort>
-    {order.slice(0, maxReviews).map(review => (
-     <ul key={review.review_id}>
-     <ReviewTile review={review}></ReviewTile>
-     </ul>
-    ))}
-    <div className="mt-4 flex gap-4">
-    <NewReview product={product} setReviews={setReviews} order={order} setOrder={setOrder}></NewReview>
-    <MoreReviews reviews={reviews} maxReviews={maxReviews} setMaxReviews={setMaxReviews}></MoreReviews>
+    <div>
+    <div className="overflow-y-scroll max-h-screen scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100">
+        <div className="mb-4">
+          <Sort reviews={reviews} setReviews={setReviews} order={order} setOrder={setOrder}></Sort>
+          {order.slice(0, maxReviews).map(review => (
+          <ul key={review.review_id}>
+          <ReviewTile review={review}></ReviewTile>
+          </ul>
+          ))}
     </div>
+    </div>
+    <div className="mt-4 flex gap-4">
+          <NewReview product={product} setReviews={setReviews} order={order} setOrder={setOrder}></NewReview>
+          <MoreReviews reviews={reviews} maxReviews={maxReviews} setMaxReviews={setMaxReviews}></MoreReviews>
+        </div>
     </div>
   )
 }

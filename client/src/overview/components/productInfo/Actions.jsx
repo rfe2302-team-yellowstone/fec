8ab2b47a97cad5abacd4b99d53b4a271f5ede420
Stephen Perkins <stperk21@gmail.com>
2023-03-25 09:32:39 -0400

    Social media buttons and SVG asset management

--- client/src/overview/components/productInfo/Actions.jsx
+++ client/src/overview/components/productInfo/Actions.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
-import AddToCartButton from './AddToCartButton.jsx'
-// import FacebookLogo from '../../../assets/facebook.svg'
+import AddToCartButton from '../../../assets/add-to-cart.svg'
+import FacebookLogo from '../../../assets/facebook.svg'
+import TwitterLogo from '../../../assets/twitter.svg'
+import PinterestLogo from '../../../assets/pinterest.svg'
+import ShareLogo from '../../../assets/share.svg'
+import FavoriteLogo from '../../../assets/favorite.svg'
 
 export default function Actions () {
 
-  const socials = [
-    {
-      name: 'facebook',
-      url: ''
-    }
-  ]
 
   return (
     <div className='self-center flex flex-col items-center'>
-      <p> Actions </p>
-      <AddToCartButton />
+      <p className='basis-full'> Actions </p>
+      <div className='flex flex-row items-center space-x-2 '>
 
-      {/* <div id='overview-facebook-link' className="btn">
-        <img src={FacebookLogo} alt='Facebook Logo' />
-      </div> */}
+
+        <div id='add-to-cart-inline-button' className="btn">
+          Add To Cart
+          <img src={AddToCartButton} className='h-4 xs:h-2 sm:h-3 lg:h-5 ml-2'></img>
+        </div>
+
+        <div id='favorite-inline-button' className="btn btn-circle p-2">
+          <img src={FavoriteLogo} ></img>
+        </div>
+
+        <div id='facebook-inline-button' className="btn btn-circle">
+          <img src={FacebookLogo} ></img>
+        </div>
+
+        <div id='twitter-inline-button' className="btn btn-circle p-2">
+          <img src={TwitterLogo} ></img>
+        </div>
+
+        <div id='pinterest-inline-button' className="btn btn-circle p-2">
+          <img src={PinterestLogo} ></img>
+        </div>
+
+        <div id='share-inline-button' className="btn btn-circle p-2">
+          <img src={ShareLogo} ></img>
+        </div>
+
+
+      </div>
 
     </div>
   )
--- client/src/overview/components/productInfo/AddToCartButton.jsx
+++ client/src/overview/components/productInfo/AddToCartButton.jsx
@@ -5,8 +5,10 @@ export default function AddToCartButton () {
     // <div className="btn flex-1 basis-2">
     //   Add to cart
     // </div>
+
+
     <div id='overview-add-to-cart-button'
-      className="btn"
+      className="btn "
       >
         Add to cart
         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">

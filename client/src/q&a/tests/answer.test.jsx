79ca9f5ae65a272994831f1c2dd3d9074282a382
Zach Timmerman <zachary.timmerman@gmail.com>
2023-03-25 11:05:47 -0400

    Add jest to env property in eslintrc.json

---
+++ client/src/q&a/tests/answer.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Answer from '../components/answer.jsx';
+
+describe('Answer', () => {
+
+  test('', () => {
+
+  })
+})

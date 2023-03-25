e4c5bfb1570c618d56aefc05cae5ec9c1ba3228d
Zach Timmerman <zachary.timmerman@gmail.com>
2023-03-25 09:55:46 -0400

    Integrated Cloudinary Upload Widget into Answer Form Modal component, converts uploaded images to cloudinary URLs

--- client/src/q&a/components/qa-header.jsx
+++ client/src/q&a/components/qa-header.jsx
@@ -4,7 +4,7 @@ import QAndASearch from './qa-search.jsx';
 const QAndAHeader = ({questions, setQuestions, allQuestions, tempQuestions, setTempQuestions}) => {
   return (
     <header className='my-2 w-[60rem]'>
-      <h4>QUESTIONS & ANSWERS</h4>
+      <h4 className='font-bold'>QUESTIONS & ANSWERS</h4>
       <QAndASearch questions={questions} setQuestions={setQuestions} allQuestions={allQuestions} tempQuestions={tempQuestions} setTempQuestions={setTempQuestions}/>
     </header>
   )

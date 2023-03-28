968a3e9d3e4ba14d77e623c344eb173e8b4920ec
Zach Timmerman <zachary.timmerman@gmail.com>
2023-03-28 19:19:49 -0400

    Begin framing of Question Form Modal component unit tests

---
+++ client/src/q&a/tests/question-form-modal.test.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import QuestionFormModal from '../components/question-form-modal.jsx';
+
+jest.mock('axios');
+
+describe('Question Form Modal', () => {
+  const fullscreenImageURL = 'https://www.cnet.com/a/img/resize/b7707b8ad323bc77e6c9baca0e0950097fdb40f0/hub/2020/11/05/27b4a2c6-e262-4136-8edc-ceb2715371a2/guardians1.jpg?auto=webp&fit=crop&height=675&width=1200';
+
+  test('closes the modal when user clicks on X button', () => {
+    const isModalOpen = false;
+    const setIsModalOpen = jest.fn();
+    const productId = 37311;
+    const productName = 'Camo Onesie';
+
+    const TestComponent = () => {
+      const [isModalOpen, setIsModalOpen] = useState(true);
+
+      return (
+        <QuestionFormModal setIsModalOpen={setIsModalOpen} isModalOpen={isModalOpen} productId={productId} productName={productName}/>
+      );
+    }
+
+    render(<TestComponent />);
+
+    fireEvent.click(screen.getByText('X'));
+
+    expect(screen.getByRole('dialog')).not.toHaveClass('modal-open');
+  })
+
+  test('submits the input values when submit button is clicked', async () => {
+    const isModalOpen = false;
+    const setIsModalOpen = jest.fn();
+    const productId = 37311;
+    const productName = 'Camo Onesie';
+
+    axios.post.mockResolvedValue({
+      data: {},
+      status: 201,
+      statusText: 'OK',
+      headers: {},
+      config: {},
+      request: {}
+    })
+
+    const TestComponent = () => {
+      const [isModalOpen, setIsModalOpen] = useState(true);
+
+      return (
+        <QuestionFormModal setIsModalOpen={setIsModalOpen} isModalOpen={isModalOpen} productId={productId} productName={productName}/>
+      );
+    }
+
+    render(<TestComponent />);
+
+    fireEvent.change(screen.getByLabelText('Your Question'), {target: {value: 'Does this fit?'}});
+    fireEvent.change(screen.getByLabelText('Your Nickname'), {target: {value: 'testperson123'}});
+    fireEvent.change(screen.getByLabelText('Your email', {target: {value: 'test@example.com'}}));
+    fireEvent.click(screen.getByRole('button', {name: 'Submit Question'}));
+
+    await waitFor(() => {
+      expect(axios.post).toHaveBeenCalledWith('http://localhost:3000/qa/questions');
+    });
+  })
+
+})

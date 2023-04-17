f64a1c78b2ea023895fad3dfd85dff4f8f4c05fb
Zach Timmerman <zachary.timmerman@gmail.com>
2023-04-17 01:35:58 -0400

    Add static file compression

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require("express");
 const path = require("path");
 const router = require('./routes.js').router
 const cors = require('cors')
+var compression = require('compression')
+var expressStaticGzip = require("express-static-gzip");
 
 const app = express();
 let port = process.env.PORT || 3000;
@@ -10,10 +12,12 @@ let port = process.env.PORT || 3000;
 const morgan = require('morgan')
 
 // Serves up all static and generated assets in ../client/dist
-app.use(express.static(path.join(__dirname, "/../client/dist")));
+// app.use(express.static(path.join(__dirname, "/../client/dist")));
+app.use('/', expressStaticGzip(path.join(__dirname, "/../client/dist")));
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(compression());
 
 app.use('/', router)
 
